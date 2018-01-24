739052a20ae6ef874e9b8ef6247d48cd275d3af7
Mo Adib <mohamadfiore@gmail.com>
2018-01-23 22:44:13 -0800

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,19 +1,17 @@
 package oneToFive;
 
-import java.util.*;
-
 /*Write code to reverse a C-Style String. 
 (C-String means that “abcd” is represented as  
 five characters, including the null character.)*/
 public class Reverse {
 
-	//Using a void function
+	// Using a void return function
 	public static void reverse(String s) {
 		char[] charArray = new char[s.length()];
 
 		for (int i = s.length() - 1; i >= 0; i--) {
 
-			charArray[s.length()-i-1] = s.charAt(i);
+			charArray[s.length() - i - 1] = s.charAt(i);
 		}
 
 		for (char tmp : charArray) {
@@ -21,6 +19,23 @@ public class Reverse {
 		}
 		System.out.println();
 
+	}
+	
+	// Using only one data structure 
+	public static String reverseString1(String s) {
+		char[] charArray = s.toCharArray();
+		int i = 0;
+		int j = s.length() - 1;
+		char temp;
+		while (i < j) {
+			temp = charArray[j];
+			charArray[j] = charArray[i];
+			charArray[i] = temp;
+			i++;
+			j--;
+		}
+
+		return new String(charArray);
 	}
 
 	// Using two data structures
@@ -31,7 +46,7 @@ public class Reverse {
 
 		for (int i = 0; i < s.length(); i++) {
 			temp[i] = charArray[s.length() - 1 - i];
-			System.out.print(temp[i]);
+			// System.out.print(temp[i]);
 		}
 
 		String sReverse = new String(temp);
@@ -39,8 +54,9 @@ public class Reverse {
 	}
 
 	public static void main(String[] args) {
-		reverse("Test Example1");
-		reverseString("Hello");
+		reverse("Jamal");
+		System.out.println(reverseString("Oliver"));
+		System.out.print(reverseString1("Mohammad"));
 
 	}
 
